09bbf03a4e90dc4cc60b3230095b0a71726ac4bc
Thamsanqa Emmanuel <ethamsanqa@gmail.com>
2025-03-16 20:30:50 +0200

    Calculator Assignment

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+# Get user input 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Enter the Arithmetic operator, e.g (+, -, *, /): ")
+
+# Calculation Code
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error! Division by zero.")
+else:
+    print("Invalid operation!")
